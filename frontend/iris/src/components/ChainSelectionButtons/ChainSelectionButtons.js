9340751eb4b2e81e96f6d3005d56fbbfb001985f
Jordan Dialpuri <44945647+Dialpuri@users.noreply.github.com>
2023-11-15 17:57:07 +0000

    Removed old dir

--- frontend/iris/src/components/ChainSelectionButtons/ChainSelectionButtons.js
+++
@@ -1,7 +0,0 @@
-import { Fragment as _Fragment, jsxDEV as _jsxDEV } from "react/jsx-dev-runtime";
-const _jsxFileName = "/Users/dialpuri/Development/iris-webapp/frontend/iris/src/components/ChainSelectionButtons/ChainSelectionButtons.tsx";
-export default function ChainSelectionButtons(props) {
-    return (_jsxDEV("div", { className: 'flex transition-al text-center align-center justify-center', children: props.chainListSet !== true ? _jsxDEV(_Fragment, { children: "Loading..." }, void 0, false, { fileName: _jsxFileName, lineNumber: 6, columnNumber: 41 }, this) : props.chainList.map((item, index) => {
-            return (_jsxDEV("button", { className: item == props.selectedChain ? 'mx-2 w-12 h-12 text-center align-middle px-1 py-1 rounded-md text-primary border-2 bg-secondary hover:bg-tertiary' : 'mx-2 w-12 h-12 rounded-md text-center align-middle px-1 py-1 border-2 text-primary bg-gray hover:bg-gray', onClick: () => { props.setSelectedChain(item); }, children: item }, index, false, { fileName: _jsxFileName, lineNumber: 7, columnNumber: 21 }, this));
-        }) }, void 0, false, { fileName: _jsxFileName, lineNumber: 4, columnNumber: 13 }, this));
-}
--- frontend/iris/src/components/Loading/Loading.js
+++
@@ -1,6 +0,0 @@
-import { jsxDEV as _jsxDEV } from "react/jsx-dev-runtime";
-const _jsxFileName = "/Users/dialpuri/Development/iris-webapp/frontend/iris/src/components/Loading/Loading.tsx";
-import ClipLoader from "react-spinners/ClipLoader";
-export default function Loading(props) {
-    return (_jsxDEV("div", { className: "flex flex-col items-center justify-center px-12 m-12 h-64 min-w-64 border-2 border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 dark:border-gray-600", children: [_jsxDEV("h3", { className: "my-6", children: props.loadingText }, void 0, false, { fileName: _jsxFileName, lineNumber: 7, columnNumber: 13 }, this), _jsxDEV(ClipLoader, { loading: true, size: 100, "aria-label": "Loading Spinner", "data-testid": "loader" }, void 0, false, { fileName: _jsxFileName, lineNumber: 8, columnNumber: 13 }, this)] }, void 0, true, { fileName: _jsxFileName, lineNumber: 4, columnNumber: 13 }, this));
-}
